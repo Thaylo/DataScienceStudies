76b1ed418c1d1d839d8f6364b307d551570da90c
Thaylo Xavier <thayloxavier@gmail.com>
2019-02-19 22:52:22 -0300

    First regression working

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ import requests
 from src import api_v1_consumer as apiv1
 
 
-def build_complete_query(batch_size=1):
+def build_complete_query(starting_index=0, batch_size=36):
     url = apiv1.base_url + apiv1.listings
     default_query_attributes = apiv1.default_request_attributes
-    default_query_attributes["size"] = batch_size
-    print("Corrected size = " + str(default_query_attributes["size"]))
+    default_query_attributes["from"] = str(starting_index)
+    default_query_attributes["size"] = str(batch_size)
     for key in default_query_attributes:
         url = url + str(key) + "=" + str(default_query_attributes[key]) + "&"
     return url[:-1]
@@ -16,25 +16,57 @@ def discover_available_items_from_api():
     url = build_complete_query()
     r = requests.get(url)
     json_obj = r.json()
-    return json_obj['search']['totalCount']
+    return json_obj['search']['totalCount'] + json_obj['developments']['search']['totalCount']
+
+
+def obtain_id_price_and_area(listing):
+    pricing_info = listing["pricingInfos"][-1]
+    if (str(pricing_info['businessType']) == 'SALE') and (str(listing['showPrice']) == 'True'):
+        unit_id = listing['id']
+        unit_price = pricing_info['price']
+        unit_usable_area = listing['usableAreas'][-1]
+        return [str(unit_id), str(unit_price), str(unit_usable_area)]
+    else:
+        return [str(-1), str(0), str(0)]
 
 
 def obtain_data_from_api():
-    #total_count = discover_available_items_from_api()
-    total_count = 36 #discover_available_items_from_api()
-    huge_url = build_complete_query(total_count)
-    r = requests.get(huge_url)
-    json_obj = r.json()
+    count = 0
+    cursor_index = 0
+    main_batch_size = 36
 
-    i = 1
-    for value in json_obj['search']['result']['listings']:
-        print(value)
-        print(i)
-        i = i+1
+    total_count = discover_available_items_from_api()
+    listings = []
+
+    while count < total_count:
+        query_url = build_complete_query(cursor_index, main_batch_size)
+        r = requests.get(query_url)
+        json_obj = r.json()
+
+        for item in json_obj['developments']['search']['result']['listings']:
+            observation = obtain_id_price_and_area(item['listing'])
+            if observation[0] != "-1":
+                listings = listings + [observation]
+        for item in json_obj['search']['result']['listings']:
+            observation = obtain_id_price_and_area(item['listing'])
+            if observation[0] != "-1":
+                listings = listings + [observation]
+
+        count += main_batch_size
+        cursor_index += main_batch_size
+    return listings
+
+
+def dump_data(filename, data):
+    with open(filename, 'w') as output_file:
+        for datum in data:
+            output_file.write(str(datum) + "\n")
 
 
 def main():
-    obtain_data_from_api()
+    filename = "vivareal_enseada_do_sua.txt"
+    listings = obtain_data_from_api()
+    dump_data(filename, listings)
 
 
 if __name__ == "__main__":
